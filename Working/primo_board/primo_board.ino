a9fd27870fd5e7922898fd3245451b8f636586b7
Emerson Ribeiro de Mello <emersonmello@users.noreply.github.com>
2018-04-21 19:45:57 -0300

    Minor fix to not allow a function block on function lane

--- Working/primo_board/primo_board.ino
+++ Working/primo_board/primo_board.ino
@@ -1,36 +1,40 @@
 /*
-http://primo.io
+  http://primo.io
 
-This sketch is part of the Primo Prototype Documentation.
-http://docs.primo.io
+  This sketch is part of the Primo Prototype Documentation.
+  http://docs.primo.io
 
-Tested on the Arduino Mega 2560.
-Load this into the Interface Board.
+  Tested on the Arduino Mega 2560.
+  Load this into the Interface Board.
 
-ANALOG VALUES FOR DIFFERENT RESISTORS:
-10KΩ = 500
-100KΩ = 920
-4.7KΩ = 319
-220Ω = 20
+  ANALOG VALUES FOR DIFFERENT RESISTORS:
+  10KΩ = 500
+  100KΩ = 920
+  4.7KΩ = 319
+  220Ω = 20
 */
 
 //4.7kΩ RED Block
-int forwardVal = 326;
+const int forwardVal = 326;
 //220Ω Yellow Block
-int rightVal = 22;
+const int rightVal = 22;
 //10kΩ Green Block
-int functionVal = 511;
+const int functionVal = 511;
 //100kΩ Blue Block
-int leftVal = 931;
+const int leftVal = 931;
+
+const int emptyEdge = 1010;
 
 //range
-int gap = 50;
+const int gap = 50;
 
-int buttonPin = 50;
+const int buttonPin = 50;
+
 int butVal, pButVal;
 
 boolean isExecuting = false;
 boolean wrongInstruction = false;
+boolean thereIsFunctionBlock = false;
 
 int limit = 12;
 
@@ -45,123 +49,137 @@ void setup() {
   pinMode(buttonPin, INPUT);
   Serial.begin(9600);
   pinMode(13, OUTPUT);
-  digitalWrite(13,LOW);
+  digitalWrite(13, LOW);
 
 }
 
 /* blink all leds to say: wrong sequence, man!*/
-void wrongFeedback(){
+void wrongFeedback() {
   Serial.write('W'); // sending Wrong message to cubetto
   int led = 30;
-  for (int i=0; i < 16; i++) {
-    digitalWrite(i+led, LOW);
+  for (int i = 0; i < 16; i++) {
+    digitalWrite(i + led, LOW);
   }
-  for(int j=0; j< 3; j++){
-    for (int i=0; i < 4; i++) {
-      digitalWrite(i+led, HIGH);
-      digitalWrite(7-i+led, HIGH);
-      digitalWrite(i+8+led, HIGH);
-      digitalWrite(i+12+led, HIGH);
+  for (int j = 0; j < 3; j++) {
+    for (int i = 0; i < 4; i++) {
+      digitalWrite(i + led, HIGH);
+      digitalWrite(7 - i + led, HIGH);
+      digitalWrite(i + 8 + led, HIGH);
+      digitalWrite(i + 12 + led, HIGH);
       delay(100);
     }
-    for (int i=3; i >=0 ; i--) {
-      digitalWrite(i+led, LOW);
-      digitalWrite(7-i+led, LOW);
-      digitalWrite(i+8+led, LOW);
-      digitalWrite(i+12+led, LOW);
+    for (int i = 3; i >= 0 ; i--) {
+      digitalWrite(i + led, LOW);
+      digitalWrite(7 - i + led, LOW);
+      digitalWrite(i + 8 + led, LOW);
+      digitalWrite(i + 12 + led, LOW);
       delay(100);
     }
   }
 }
 
-void turnOnLED(int i, int led){
-  if (vals[i] < 1010) {
-      digitalWrite(led, HIGH);
-    }
-    else {
-      digitalWrite(led, LOW);
-    }
+void turnOnLED(int i, int led) {
+  if (vals[i] < emptyEdge) {
+    digitalWrite(led, HIGH);
+  }
+  else {
+    digitalWrite(led, LOW);
+  }
 }
 
-boolean isAFunctionBlock(int pos){
-  if ((vals[pos] > (functionVal - gap)) && (vals[pos] < (functionVal + gap))){
-    return true;
-  }
-  return false;
+boolean isAFunctionBlock(int pos) {
+  return (whichBlock(vals[pos]) == 'P') ? true : false;
 }
 
 void readAnalogicPins() {
-    limit = 12;
-  for(int i = 0; i < 16; i++) {
+  limit = 12;
+  for (int i = 0; i < 16; i++) {
     vals[i] = analogRead(i);
-    if ((i<12) && (isAFunctionBlock(i)) ){
-        limit = 16;
+    if ((i < 12) && (isAFunctionBlock(i)) ) {
+      limit = 16;
     }
   }
 }
 
+char whichBlock(int value) {
+  //forward - Red
+  if (value > (forwardVal - gap) && value < (forwardVal + gap)) {
+    return 'F';
+  }
+  //left - Blue
+  if (value > (leftVal - gap) && value < (leftVal + gap)) {
+    return 'L';
+  }
+  //right - Yellow
+  if (value > (rightVal - gap) && value < (rightVal + gap)) {
+    return 'R';
+  }
+  //function - Green
+  if (value > (functionVal - gap) && value < (functionVal + gap)) {
+    return 'P'; // function or procedure
+  }
+  return 'U'; // unknown
+}
+
+
 void loop() {
 
+  thereIsFunctionBlock = false;
   wrongInstruction = false;
 
   readAnalogicPins();
 
   //turn on LEDs if a block is inserted
-  for(int i = 0; i < 16; i++) {
-    int led = i+30;
+  for (int i = 0; i < 16; i++) {
+    int led = i + 30;
     turnOnLED(i, led);
-    if ((i>=12) && (limit == 12)){
-        digitalWrite(led, LOW);
+    if (i < 12) { // regular line
+      if (isAFunctionBlock(i)) {
+        thereIsFunctionBlock = true;
+      }
     }
   }
+
   delay(100);
 
-  boolean previousFunctionBlock = false;
-
   // blink leds out of sequence
-  for(int i = 0; i < limit; i++) {
-    int led = i+30;
-    if ((i > 0) && (i < 12)){ // regular line
-      if ((vals[i] < 1010 ) && (vals[i-1] >= 1010)){
+  for (int i = 0; i < 16; i++) {
+    int led = i + 30;
+    // blink the current led if there is no previous block inserted
+    if ((i > 0) && (i < 16)) { // regular line
+      if ((vals[i] < emptyEdge ) && (vals[i - 1] >= emptyEdge)) {
+        if ((i != 12) || ((i==12) && (!thereIsFunctionBlock))){
+          digitalWrite(led, LOW);
+          wrongInstruction = true;
+        }
+      }
+    }
+
+    if (i > 11) { // function area (green line)
+      if ((isAFunctionBlock(i)) || (!thereIsFunctionBlock)) {
         digitalWrite(led, LOW);
         wrongInstruction = true;
       }
-    }else if (i>12){ // function area (green line)
-
-          // a previous function block will not allow execution of next blocks
-          if (isAFunctionBlock(i-1)){
-              previousFunctionBlock = true;
-          }
-          // blink leds of current block if previous block is a function block or block is out of sequence
-          if (((vals[i] < 1010 ) && (vals[i-1] >= 1010)) || (previousFunctionBlock)){
-            digitalWrite(led, LOW);
-            wrongInstruction = true;
-          }
     }
-  }
 
-  // first position on function line should not be a function block because it will result in a infinite loop
-  if (isAFunctionBlock(12)){
-    digitalWrite(12+30, LOW);
-    wrongInstruction = true;
   }
 
   butVal = digitalRead(buttonPin);
 
   //if button press & not executing
-  if(butVal == HIGH && pButVal == LOW && isExecuting == false) {
+  if (butVal == HIGH && pButVal == LOW && isExecuting == false) {
     isExecuting = true;
   }
 
-  if(isExecuting) {
+  if (isExecuting) {
 
-    if (wrongInstruction){
+    if (wrongInstruction) {
       // TODO: shake cubetto to say: no no no no
       wrongFeedback();
-    }else{
+    } else {
       readPins();
       for ( int i = 0; i < 12; i++) {
-        if (vals[i] < 1010) {
+        if (vals[i] < emptyEdge) {
           route(i);
         }
         else {
--- Working/primo_board/route.ino
+++ Working/primo_board/route.ino
@@ -1,57 +1,41 @@
 void route(int i) {
-
   int led = i + 30;
 
-  //forward
-  if (vals[i] > (forwardVal - gap) && vals[i] < (forwardVal + gap)) {
-   // Serial1.print('F');
-    Serial.println('F');
-    digitalWrite(led, LOW);
-    delay(1500);
-  }
-
-  //left
-  if (vals[i] > (leftVal - gap) && vals[i] < (leftVal + gap)) {
- //   Serial1.print('L');
-    Serial.println('L');
-    digitalWrite(led, LOW);
-    delay(1000);
-  }
-
-
-  //right
-  if (vals[i] > (rightVal - gap) && vals[i] < (rightVal + gap)) {
-  //  Serial1.print('R');
-    Serial.println('R');
-    digitalWrite(led, LOW);
-    delay(1000);
-  }
-
-  //function
-  if (vals[i] > (functionVal - gap) && vals[i] < (functionVal + gap)) {
-    digitalWrite(led, LOW);
-    //route function line
-    for ( int j = 12; j < 16; j++) {
-      if (vals[j] < 1023) {
-          route(j);
-      }else {
-        break;
+  switch (whichBlock(vals[i])){
+    case 'F':
+      Serial.println('F');
+      digitalWrite(led, LOW);
+      delay(1500);
+      break;
+    case 'L':
+      Serial.println('L');
+      digitalWrite(led, LOW);
+      delay(1000);
+      break;
+    case 'R':
+      Serial.println('R');
+      digitalWrite(led, LOW);
+      delay(1000);
+      break;
+    case 'P':
+      digitalWrite(led, LOW);
+      //route function line
+      for ( int j = 12; j < 16; j++) {
+        switch (whichBlock(vals[j])){
+          case 'F':
+          case 'L':
+          case 'R':
+            route(j);
+            break;
+        }
       }
-    }
-    
-    //turn function leds back on
-    for (int j = 12; j < 16; j++) {
-      if (vals[j] < 1023) {
-        digitalWrite(j + 30, HIGH);
+      //turn function leds back on
+      for (int j = 12; j < 16; j++) {
+        if (vals[j] < emptyEdge) { //1023 or 1010?
+          digitalWrite(j + 30, HIGH);
+        }
       }
-    }
-    delay(500);
+      delay(500);
+      break;
   }
 }
-
-
-
-
-
-
-
