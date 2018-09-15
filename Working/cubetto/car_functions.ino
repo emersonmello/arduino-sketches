8d75743c919435e8e1482b3f6bffb8bdc95cdc4e
Emerson Ribeiro de Mello <emersonmello@users.noreply.github.com>
2018-09-15 17:40:29 -0300

    fixing indenting following Arduino IDE convention

--- Working/cubetto/car_functions.ino
+++ Working/cubetto/car_functions.ino
@@ -6,168 +6,168 @@
 const int whiteSlice = 1;
 const int blackSlice = 0;
 
-int readEncoder(int enc){
-    int value = analogRead(enc);
-    return (value < bwVal) ? blackSlice : whiteSlice;
+int readEncoder(int enc) {
+  int value = analogRead(enc);
+  return (value < bwVal) ? blackSlice : whiteSlice;
 }
 
 //rotate wheels until the next black sign
-void initialize(){
-    //forward left
-    analogWrite(leftForward, 88);
-    digitalWrite(leftReverse, LOW);
+void initialize() {
+  //forward left
+  analogWrite(leftForward, 88);
+  digitalWrite(leftReverse, LOW);
 
 #ifdef DEBUG
-    print_inline_encoders();
+  print_inline_encoders();
 #endif
-    while (readEncoder(leftEncoder) == blackSlice);
+  while (readEncoder(leftEncoder) == blackSlice);
 
-    stopLeft();
+  stopLeft();
 
-    //forward right
-    analogWrite(rightForward, 88);
-    digitalWrite(rightReverse, LOW);
+  //forward right
+  analogWrite(rightForward, 88);
+  digitalWrite(rightReverse, LOW);
 
-    while (readEncoder(rightEncoder) == blackSlice);
+  while (readEncoder(rightEncoder) == blackSlice);
 
-    stopRight();
+  stopRight();
 #ifdef DEBUG
-    print_inline_encoders();
+  print_inline_encoders();
 #endif
 }
 
 /* MOTION FUNCTIONS */
 
 // Red block
-void forward(int speed, int steps){
-    int counter = 0;
-    int value = -1;
-    int previousValue = -1;
+void forward(int speed, int steps) {
+  int counter = 0;
+  int value = -1;
+  int previousValue = -1;
 
-    int vL = -1;
-    int pvL = -1;
-    int counterL = 0;
+  int vL = -1;
+  int pvL = -1;
+  int counterL = 0;
 
-    int vR = -1;
-    int pvR = -1;
-    int counterR = 0;
+  int vR = -1;
+  int pvR = -1;
+  int counterR = 0;
 
-    pvL = readEncoder(leftEncoder);
-    pvR = readEncoder(rightEncoder);
+  pvL = readEncoder(leftEncoder);
+  pvR = readEncoder(rightEncoder);
 
 #ifdef DEBUG
-    init_debug_counters();
+  init_debug_counters();
 #endif
 
-    //left forward
-    analogWrite(leftForward, speed);
-    digitalWrite(leftReverse, LOW);
+  //left forward
+  analogWrite(leftForward, speed);
+  digitalWrite(leftReverse, LOW);
 
-    //right forward
-    //ToDo My right motor is faster than left one. Why? I don't know.
-    // Workaround: set it a little bit slow
-    analogWrite(rightForward, (speed - 30));
-    digitalWrite(rightReverse, LOW);
+  //right forward
+  //ToDo My right motor is faster than left one. Why? I don't know.
+  // Workaround: set it a little bit slow
+  analogWrite(rightForward, (speed - 30));
+  digitalWrite(rightReverse, LOW);
 
-    while (counterL <= steps || counterR <= steps){
-        if (counterL <= steps){
-            vL = readEncoder(leftEncoder);
-            if (vL != pvL){
-                counterL++;
-                pvL = vL;
-            }
-        }
-        if (counterR <= steps){
-            vR = readEncoder(rightEncoder);
-            if (vR != pvR){
-                counterR++;
-                pvR = vR;
-            }
-        }
-#ifdef DEBUG
-        count_wheel_turns();
-#endif
+  while (counterL <= steps || counterR <= steps) {
+    if (counterL <= steps) {
+      vL = readEncoder(leftEncoder);
+      if (vL != pvL) {
+        counterL++;
+        pvL = vL;
+      }
     }
-
-    stop();
+    if (counterR <= steps) {
+      vR = readEncoder(rightEncoder);
+      if (vR != pvR) {
+        counterR++;
+        pvR = vR;
+      }
+    }
 #ifdef DEBUG
-    print_count_wheel_turns();
+    count_wheel_turns();
+#endif
+  }
+
+  stop();
+#ifdef DEBUG
+  print_count_wheel_turns();
 #endif
 }
 
 // direction = (L)eft or (R)ight
 // Left = Blue block
 // Right = Yellow block
-void turn(int speed, int steps, char direction){
-    int counter = 0;
-    int value = -1;
-    int previousValue = -1;
-    int encoder = -1;
+void turn(int speed, int steps, char direction) {
+  int counter = 0;
+  int value = -1;
+  int previousValue = -1;
+  int encoder = -1;
 
 #ifdef DEBUG
-    init_debug_counters();
+  init_debug_counters();
 #endif
 
-    if (direction == LEFT){
-        encoder = leftEncoder;
-        DEBUG_PRINT("Turn LEFT");
+  if (direction == LEFT) {
+    encoder = leftEncoder;
+    DEBUG_PRINT("Turn LEFT");
 
-        //left backward
-        digitalWrite(leftForward, LOW);
-        analogWrite(leftReverse, speed);
+    //left backward
+    digitalWrite(leftForward, LOW);
+    analogWrite(leftReverse, speed);
 
-        //right forward
-        analogWrite(rightForward, speed);
-        digitalWrite(rightReverse, LOW);
+    //right forward
+    analogWrite(rightForward, speed);
+    digitalWrite(rightReverse, LOW);
+  }
+  else {
+    encoder = rightEncoder;
+    DEBUG_PRINT("Turn RIGHT");
+
+    //right reverse
+    digitalWrite(rightForward, LOW);
+    analogWrite(rightReverse, speed);
+
+    //left forward
+    digitalWrite(leftReverse, LOW);
+    analogWrite(leftForward, speed);
+  }
+  //count rotation
+  while (counter <= steps) {
+    value = readEncoder(encoder);
+    if (value != previousValue) {
+      counter++;
+      previousValue = value;
     }
-    else{
-        encoder = rightEncoder;
-        DEBUG_PRINT("Turn RIGHT");
-
-        //right reverse
-        digitalWrite(rightForward, LOW);
-        analogWrite(rightReverse, speed);
-
-        //left forward
-        digitalWrite(leftReverse, LOW);
-        analogWrite(leftForward, speed);
-    }
-    //count rotation
-    while (counter <= steps){
-        value = readEncoder(encoder);
-        if (value != previousValue){
-            counter++;
-            previousValue = value;
-        }
 #ifdef DEBUG
-        count_wheel_turns();
+    count_wheel_turns();
 #endif
-    }
-    stop();
+  }
+  stop();
 #ifdef DEBUG
-    print_count_wheel_turns();
+  print_count_wheel_turns();
 #endif
 }
 
 /* STOP FUNCTIONS */
-void stopLeft(){
-    digitalWrite(leftForward, LOW);
-    digitalWrite(leftReverse, LOW);
+void stopLeft() {
+  digitalWrite(leftForward, LOW);
+  digitalWrite(leftReverse, LOW);
 }
 
-void stopRight(){
-    digitalWrite(rightForward, LOW);
-    digitalWrite(rightReverse, LOW);
+void stopRight() {
+  digitalWrite(rightForward, LOW);
+  digitalWrite(rightReverse, LOW);
 }
 
-void stop(){
-    stopLeft();
-    stopRight();
+void stop() {
+  stopLeft();
+  stopRight();
 }
 
-void hardStop(){
-    digitalWrite(leftForward, HIGH);
-    digitalWrite(leftReverse, HIGH);
-    digitalWrite(rightForward, HIGH);
-    digitalWrite(rightReverse, HIGH);
+void hardStop() {
+  digitalWrite(leftForward, HIGH);
+  digitalWrite(leftReverse, HIGH);
+  digitalWrite(rightForward, HIGH);
+  digitalWrite(rightReverse, HIGH);
 }
--- Working/cubetto/cubetto.ino
+++ Working/cubetto/cubetto.ino
@@ -59,106 +59,106 @@ int d_counterR = 0;
 int d_counterL = 0;
 int d_i_ts = 0;
 
-void init_debug_counters(){
-    d_i_ts = millis();
-    d_valueL = -1;
-    d_previousValueL = -1;
-    d_valueR = -1;
-    d_previousValueR = -1;
-    d_counterR = 0;
-    d_counterL = 0;
-    d_previousValueL = readEncoder(leftEncoder);
-    d_previousValueR = readEncoder(rightEncoder);
+void init_debug_counters() {
+  d_i_ts = millis();
+  d_valueL = -1;
+  d_previousValueL = -1;
+  d_valueR = -1;
+  d_previousValueR = -1;
+  d_counterR = 0;
+  d_counterL = 0;
+  d_previousValueL = readEncoder(leftEncoder);
+  d_previousValueR = readEncoder(rightEncoder);
 }
 
-void count_wheel_turns(){
-    d_valueR = readEncoder(rightEncoder);
-    d_valueL = readEncoder(leftEncoder);
-    if (d_valueL != d_previousValueL){
-        d_counterL++;
-        d_previousValueL = d_valueL;
-    }
-    if (d_valueR != d_previousValueR){
-        d_counterR++;
-        d_previousValueR = d_valueR;
-    }
+void count_wheel_turns() {
+  d_valueR = readEncoder(rightEncoder);
+  d_valueL = readEncoder(leftEncoder);
+  if (d_valueL != d_previousValueL) {
+    d_counterL++;
+    d_previousValueL = d_valueL;
+  }
+  if (d_valueR != d_previousValueR) {
+    d_counterR++;
+    d_previousValueR = d_valueR;
+  }
 }
 
-void print_count_wheel_turns(){
-    int ts = millis() - d_i_ts;
-    Serial.print("Elapsed time: ");
-    Serial.print(ts);
-    Serial.print(", counterL : ");
-    Serial.print(d_counterL);
-    Serial.print(", counterR : ");
-    Serial.print(d_counterR);
-    Serial.print(" -> ");
-    print_inline_encoders();
+void print_count_wheel_turns() {
+  int ts = millis() - d_i_ts;
+  Serial.print("Elapsed time: ");
+  Serial.print(ts);
+  Serial.print(", counterL : ");
+  Serial.print(d_counterL);
+  Serial.print(", counterR : ");
+  Serial.print(d_counterR);
+  Serial.print(" -> ");
+  print_inline_encoders();
 }
 
 #endif
 
-void setup(){
-    pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, HIGH);
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
 
-    Serial.begin(9600);
+  Serial.begin(9600);
 
-    //initialize left
-    pinMode(leftEnable, OUTPUT);
-    pinMode(leftReverse, OUTPUT);
-    pinMode(leftForward, OUTPUT);
+  //initialize left
+  pinMode(leftEnable, OUTPUT);
+  pinMode(leftReverse, OUTPUT);
+  pinMode(leftForward, OUTPUT);
 
-    //initialize right
-    pinMode(rightEnable, OUTPUT);
-    pinMode(rightForward, OUTPUT);
-    pinMode(rightReverse, OUTPUT);
+  //initialize right
+  pinMode(rightEnable, OUTPUT);
+  pinMode(rightForward, OUTPUT);
+  pinMode(rightReverse, OUTPUT);
 
-    //enable motors
-    digitalWrite(leftEnable, HIGH);
-    digitalWrite(rightEnable, HIGH);
+  //enable motors
+  digitalWrite(leftEnable, HIGH);
+  digitalWrite(rightEnable, HIGH);
 
-    delay(500);
+  delay(500);
 
-    //initialize aligns the wheels
-    initialize();
-    if (Serial.available() > 0){
-        Serial.println("Hi");
-    }
+  //initialize aligns the wheels
+  initialize();
+  if (Serial.available() > 0) {
+    Serial.println("Hi");
+  }
 }
 
-void loop(){
-    //read rom the xbee
-    if (Serial.available() > 0){
-        instruction = Serial.read();
-    }
+void loop() {
+  //read rom the xbee
+  if (Serial.available() > 0) {
+    instruction = Serial.read();
+  }
 
-    //decode instruction
-    switch (instruction){
-        case STATUS:
-        case 's':
-            print_inline_encoders();
-            break;
-        case FORWARD: // Red block
-        case 'f':
-            forward(128, 16); //128,16 ~= 12cm
-            break;
-        case LEFT: // Blue block
-        case 'l':
-            turn(128, 10, LEFT); //128,9
-            break;
-        case RIGHT: // Yellow block
-        case 'r':
-            turn(128, 10, RIGHT); //138,7
-            break;
-        case INIT:
-        case 'i':
-            initialize();
-            break;
-        case STOP:
-        case 'o':
-            stop();
-            break;
-    }
-    instruction = 'O';
+  //decode instruction
+  switch (instruction) {
+    case STATUS:
+    case 's':
+      print_inline_encoders();
+      break;
+    case FORWARD: // Red block
+    case 'f':
+      forward(128, 16); //128,16 ~= 12cm
+      break;
+    case LEFT: // Blue block
+    case 'l':
+      turn(128, 10, LEFT); //128,9
+      break;
+    case RIGHT: // Yellow block
+    case 'r':
+      turn(128, 10, RIGHT); //138,7
+      break;
+    case INIT:
+    case 'i':
+      initialize();
+      break;
+    case STOP:
+    case 'o':
+      stop();
+      break;
+  }
+  instruction = 'O';
 }
